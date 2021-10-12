4a02becce56fa9ea11fb88b6a81a4569c844088f
justinmajetich <justinmajetich@gmail.com>
2021-10-11 22:22:48 -0400

    Move dialogue tree controller to manager object

--- Assets/Scripts/CharacterInteraction.cs
+++ Assets/Scripts/CharacterInteraction.cs
@@ -1,16 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using NodeCanvas.DialogueTrees;
 
 
 public class CharacterInteraction : MonoBehaviour
 {
-    DialogueActor self;
+    DialogueActor actorSelf;
+    public DialogueManager dialogueManager;
 
     private void Start()
     {
-        self = GetComponent<DialogueActor>();
+        actorSelf = GetComponent<DialogueActor>();
+        dialogueManager = GameObject.Find("DialogueManager").GetComponent<DialogueManager>();
     }
 
     void Update()
@@ -19,26 +19,13 @@ public class CharacterInteraction : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.E))
         {
-            Debug.Log("Player: Got E Key Down");
-
             Collider2D[] hits = Physics2D.OverlapCircleAll(transform.position, 1.25f, layerMask);
-
-            Debug.Log("Player: I have " + hits.Length + " hits!");
 
             foreach (Collider2D hit in hits)
             {
-                Debug.Log("Player: Hit name is " + hit.gameObject.name);
-
                 if (hit.CompareTag("NPC"))
                 {
-                    Debug.Log("Player: I'm interacting with an NPC");
-
-                    DialogueTreeController dialogue = hit.gameObject.GetComponent<DialogueTreeController>();
-
-                    if (dialogue)
-                    {
-                        dialogue.StartDialogue(self);
-                    }
+                    dialogueManager.StartDialogueTree(actorSelf);
                 }
             }
         }
