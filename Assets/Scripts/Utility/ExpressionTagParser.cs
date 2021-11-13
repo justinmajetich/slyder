62772a82c2765236cc5da6af31f981c42f7e92c4
justinmajetich <justinmajetich@gmail.com>
2021-11-13 15:51:37 -0500

    Parse tags embedded in subtitles

---
+++ Assets/Scripts/Utility/ExpressionTagParser.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+
+public static class ExpressionTagParser
+{
+    public static event Action<int> OnSpeedExpressed;
+    public static event Action<int> OnPauseExpressed;
+    public static event Action<int> OnEmotionExpressed;
+    public static event Action<int> OnActionExpressed;
+
+    /// <summary>
+    /// Parses expression tag embedded in subtitle text, firing events accordinly.
+    /// </summary>
+    /// <param name="statement">The statement to parse.</param>
+    /// <param name="offset">The offset at which to begin parsing statement string.</param>
+    /// <returns>Returns the index of the character following tag close.</returns>
+    public static int Parse(string statement, int tagStartPosition)
+    {
+        Dictionary<string, int> tag = new Dictionary<string, int>();
+        string key = "";
+        string valueString = "";
+        int i = tagStartPosition + 1;
+
+        // Copies key from tag.
+        while (statement[i] != '=')
+        {
+            key += statement[i++];
+        }
+        i++;
+
+        // Copies value from tag.
+        while (statement[i] != Constants.ExpressionTagClose)
+        {
+            valueString += statement[i++];
+        }
+        i++;
+
+        int value = int.Parse(valueString);
+
+        switch (key)
+        {
+            case Constants.ExpressionTagSpeed:
+                OnSpeedExpressed?.Invoke(value);
+                break;
+            case Constants.ExpressionTagPause:
+                OnPauseExpressed?.Invoke(value);
+                break;
+            case Constants.ExpressionTagEmotion:
+                OnEmotionExpressed?.Invoke(value);
+                break;
+            case Constants.ExpressionTagAction:
+                OnActionExpressed?.Invoke(value);
+                break;
+        }
+
+        return i;
+    }
+}
