e4d0fc15c5d4767a0629f6c785b95059bed0a2ef
Justin <justinmajetich@gmail.com>
2022-02-08 16:27:13 -0500

    Overhaul character controller for point and click system

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -1,36 +1,164 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using NodeCanvas.DialogueTrees;
+
 
 public class CharacterController2D : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    bool isMoving = false;
-    Vector2 moveValue = Vector2.zero;
+    // Debug item.
+    public Transform targetPositionMarker;
 
-    public Sprite[] upFacingSprites = new Sprite[4];
-    public Sprite[] rightFacingSprites = new Sprite[4];
-    public Sprite[] downFacingSprites = new Sprite[4];
-    public Sprite[] leftFacingSprites = new Sprite[4];
+    [Header("Movement Settings")]
+    [SerializeField]
+    float moveSpeed = 3f;
+    Vector2 targetPosition;
+
+    [Header("Interaction Settings")]
+    public DialogueManager dialogueManager;
+    DialogueActor actorSelf;
+
+    [SerializeField, Tooltip("Distance at which an interaction can be initiated.")]
+    float interactRange = 1.0f;
+
+    RaycastHit2D hit;
+    Vector2 lastMousePosition;
+
+    [Header("Cursor Textures")]
+    [SerializeField]
+    Texture2D defaultCursor;
+    [SerializeField]
+    Texture2D interactableCursor;
+    [SerializeField]
+    Texture2D walkableCursor;
+
+
+    void OnEnable()
+    {
+        DialogueTree.OnDialogueStarted += DisablePlayerActions;
+        DialogueTree.OnDialogueFinished += EnablePlayerActions;
+    }
+
+    void OnDisable()
+    {
+        DialogueTree.OnDialogueStarted -= DisablePlayerActions;
+        DialogueTree.OnDialogueFinished -= EnablePlayerActions;
+    }
+
+    void Start()
+    {
+        if (!dialogueManager)
+        {
+            Debug.LogError("DialogueManager must be assigned.");
+            UnityEditor.EditorApplication.isPlaying = false;
+        }
+
+        actorSelf = GetComponent<DialogueActor>();
+
+        Cursor.SetCursor(defaultCursor, Vector2.zero, CursorMode.Auto);
+
+        targetPosition = transform.position;
+    }
 
     void Update()
     {
-        if (isMoving)
+        MouseHover();
+
+        if ((Vector2)transform.position != targetPosition)
         {
-            transform.Translate(moveSpeed * Time.deltaTime * new Vector2(moveValue.x, moveValue.y));
+            MoveToPosition();
         }
     }
 
-    public void OnMove(InputAction.CallbackContext context)
+    private void LateUpdate()
     {
-        if (context.started)
+        // Visualize targetPosition.
+        targetPositionMarker.position = targetPosition;
+    }
+
+    void MouseHover()
+    {
+        Vector2 currentMousePosition = Mouse.current.position.ReadValue();
+
+        // Only update hover if mouse has moved since last frame.
+        if (lastMousePosition != currentMousePosition)
         {
-            isMoving = true;
-        }
-        if (context.canceled)
-        {
-            isMoving = false;
+            hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(currentMousePosition), Vector2.zero);
+
+            // Hit interactable collider.
+            if (hit.collider && hit.collider.gameObject.layer == LayerMask.NameToLayer(Constants.LayerMaskInteractable))
+            {
+                Cursor.SetCursor(interactableCursor, Vector2.zero, CursorMode.Auto);
+            }
+            // Hit walkable collider.
+            else if (hit.collider && hit.collider.gameObject.layer == LayerMask.NameToLayer(Constants.LayerMaskWalkable))
+            {
+                Cursor.SetCursor(walkableCursor, Vector2.zero, CursorMode.Auto);
+            }
+            // No collider hit, or no relevant collider hit.
+            else
+            {
+                Cursor.SetCursor(defaultCursor, Vector2.zero, CursorMode.Auto);
+            }
         }
 
-        moveValue = context.ReadValue<Vector2>();
+        // Store current position for comparison next frame.
+        lastMousePosition = currentMousePosition;
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        // If click occured and a hovered collider has been reported...
+        if (context.performed && hit.collider)
+        {
+            // If collider is walkable, move to hit position.
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer(Constants.LayerMaskWalkable))
+            {
+                targetPosition = hit.point;
+            }
+
+            // If collider is interactable, move within range and initiate interaction.
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer(Constants.LayerMaskInteractable))
+            {
+                Vector2 interactablePosition = hit.collider.transform.position;
+
+                // If interactable is out of range, set and move to new target position within range.
+                if (Vector2.Distance(transform.position, interactablePosition) > interactRange)
+                {
+                    Vector2 dir = (Vector2)transform.position - interactablePosition;
+
+                    targetPosition = interactablePosition + (dir.normalized * interactRange);
+                }
+                
+                StartCoroutine(Interact());
+            }
+        }
+    }
+
+    void MoveToPosition()
+    {
+        transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
+    }
+
+    IEnumerator Interact()
+    {
+        // Wait for player to reach interact range if not already within range.
+        while ((Vector2)transform.position != targetPosition)
+        {
+            yield return null;
+        }
+
+        dialogueManager.StartDialogue(actorSelf, hit.collider.gameObject.GetComponent<DialogueActor>());
+    }
+
+    private void DisablePlayerActions(DialogueTree obj)
+    {
+        GetComponent<PlayerInput>().actions.Disable();
+    }
+
+    private void EnablePlayerActions(DialogueTree obj)
+    {
+        GetComponent<PlayerInput>().actions.Enable();
     }
 }
--- Assets/Scripts/Utility/Constants.cs
+++ Assets/Scripts/Utility/Constants.cs
@@ -7,4 +7,8 @@ public static class Constants
     public const string ExpressionTagEmotion = "emotion";
     public const string ExpressionTagAction = "action";
 
+    public const string LayerMaskWalkable = "Walkable";
+    public const string LayerMaskInteractable = "Interactable";
+    public const string TagWalkable = "Walkable";
+    public const string TagInteractable = "Interactable";
 }
