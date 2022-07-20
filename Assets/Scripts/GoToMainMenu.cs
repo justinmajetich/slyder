8f85df49a11402830a7518357aa7c982b7816265
Justin <justinmajetich@gmail.com>
2022-07-20 14:56:43 -0400

    Remove parameter from OnFadeToMainMenu Action

--- Assets/Scripts/GoToMainMenu.cs
+++ Assets/Scripts/GoToMainMenu.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class GoToMainMenu : MonoBehaviour
 {
     public static event Action OnCutToMainMenu;
-    public static event Action<Constants.Scene> OnFadeToMainMenu;
+    public static event Action OnFadeToMainMenu;
     public static event Action<float> OnWaitForAudioFade;
 
     public void OnMainMenu()
     {
         if (GameManager.current.GetGameState().currentScene == Constants.Scene.Credits)
         {
-            OnFadeToMainMenu?.Invoke(Constants.Scene.MainMenu);
+            OnFadeToMainMenu?.Invoke();
         }
         else
         {
