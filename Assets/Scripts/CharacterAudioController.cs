8d8c870758ae9db7fd1e94cdc4a40053fe1dd37e
Justin <justinmajetich@gmail.com>
2022-07-20 14:54:15 -0400

    Implement character dialogue sfx

--- Assets/Scripts/CharacterAudioController.cs
+++ Assets/Scripts/CharacterAudioController.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using NodeCanvas.DialogueTrees;
 
 public class CharacterAudioController : MonoBehaviour
 {
@@ -9,41 +10,82 @@ public class CharacterAudioController : MonoBehaviour
         public AudioClip[] clips = new AudioClip[10];
     }
 
+    [Serializable]
+    public class TalkClips
+    {
+        public AudioClip[] clips;
+    }
+
     AudioSource source;
+    bool isActiveActor = false;
 
     [Header("Footstep Audio")]
     public FootstepClips footstepAudio = new FootstepClips();
 
+    [Header("Talk Audio")]
+    [SerializeField]
+    string resourcePath = string.Empty;
+    public FootstepClips talkAudio = new FootstepClips();
+
     private void OnEnable()
     {
         CharacterSpriteController.OnPlayerDidStep += PlayFootstep;
+        DialogueTree.OnSubtitlesRequest += IsActiveActor;
+        SubtitleAnimator.OnTalk += PlayTalkClip;
     }
 
     private void OnDisable()
     {
         CharacterSpriteController.OnPlayerDidStep -= PlayFootstep;
+        DialogueTree.OnSubtitlesRequest -= IsActiveActor;
+        SubtitleAnimator.OnTalk -= PlayTalkClip;
     }
 
     void Start()
     {
         source = GetComponent<AudioSource>();
+
+        talkAudio.clips = Resources.LoadAll<AudioClip>(resourcePath);
+    }
+
+    void IsActiveActor(SubtitlesRequestInfo info)
+    {
+        isActiveActor = info.actor.name == GetComponentInParent<IDialogueActor>().name;
     }
 
     void PlayFootstep()
     {
-        source.clip = LoadRandomFootstep();
+        source.clip = LoadRandomClip(footstepAudio.clips);
         source.Play();
     }
 
-    AudioClip LoadRandomFootstep()
+    void PlayTalkClip(float speed)
     {
-        int randomIndex = UnityEngine.Random.Range(0, footstepAudio.clips.Length - 1);
-
-        while (source.clip == footstepAudio.clips[randomIndex])
+        if (isActiveActor && !source.isPlaying)
         {
-            randomIndex = UnityEngine.Random.Range(0, footstepAudio.clips.Length - 1);
+            source.clip = LoadRandomClip(talkAudio.clips);
+            source.Play();
+        }
+    }
+
+    AudioClip LoadRandomClip(AudioClip[] clips)
+    {
+        int size = clips.Length;
+
+        if (size <= 0)
+        {
+            return null;
         }
 
-        return footstepAudio.clips[randomIndex];
+        int randomIndex = UnityEngine.Random.Range(0, size - 1);
+
+        while (size > 1 && source.clip == clips[randomIndex])
+        {
+            randomIndex = UnityEngine.Random.Range(0, size - 1);
+        }
+
+        return clips[randomIndex];
     }
+
+
 }
